40eb0d51798fb217c5a1d7679fa1e1d8ef56f75b
Maksim Filippov <colixer@gmail.com>
2016-03-18 20:19:28 +0300

    Working determinant solution

--- matrix.rb
+++ matrix.rb
@@ -1,40 +1,28 @@
-require 'pry'
-def determinant(matrix, value = 0, depth = 0, totals = [])
-  first_row = matrix.first
-  @initial_matrix_size ||= first_row.size
-  depth += 1
+def determinant(matrix)
+  minor = []
+  minor_dets = 0
+  n = matrix[0].size
 
-  return matrix[0][0] if @initial_matrix_size == 1
-
-  return first_row if matrix.size == 1
-
-  first_row.each_with_index do |col_item, i|
-    minor_det = (i.odd? ? -1 : 1) * col_item * determinant(minor(matrix, i), value, depth, totals)
-    value += minor_det
-
-    (totals.push(minor_det) && value = 0) if depth == 1
-  end
-
-  binding.pry
-  totals.size == @initial_matrix_size ? totals.reduce(:+) : value
-end
-
-def minor(matrix, el_index)
-  minor_size = matrix.first.size - 1
-  minor_matrix = []
-
-  for i in (1..minor_size) do
-    for j in (0..minor_size) do
-      minor_matrix << matrix[i][j] if j != el_index
+  case n
+  when 1
+    return matrix[0][0]
+  when 2
+    matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1]
+  else
+    # Iterate through each element in first row
+    for i in (0...n) do
+      # Count minor matrix for element with index i
+      for j in (1...n) do
+        for k in (0...n).reject { |x| x == i }
+          minor << matrix[j][k]
+        end
+      end
+      minor = minor.each_slice(n - 1).to_a
+      minor_det = determinant(minor)
+      minor = []
+      minor_dets += (minor_det * matrix[0][i] * (i.odd? ? -1 : 1)) if minor_det.is_a? Numeric
     end
+
+    minor_dets
   end
-
-  minor_matrix.size == 1 ? minor_matrix : minor_matrix.each_slice(minor_size).to_a
 end
-
-# puts 'Should be -20'
-# puts determinant([[2,5,3], [1,-2,-1], [1,3,4]])
-puts 'Should be -1'
-puts determinant([[1,3], [2,5]])
-# puts 'Should be 1'
-# puts determinant([[1]])
